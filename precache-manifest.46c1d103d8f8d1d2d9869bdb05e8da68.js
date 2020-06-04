3e73ccfdf9cc5beac1f5603e1d8e1e03f3218b73
Alison Fahl <fahlnicolau@gmail.com>
2020-06-04 18:59:53 -0300

    Updates

---
+++ precache-manifest.46c1d103d8f8d1d2d9869bdb05e8da68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87091150df4a751f5a2e7bd8bfdef76",
+    "url": "/dgc/index.html"
+  },
+  {
+    "revision": "7bba80f7a21b750745e8",
+    "url": "/dgc/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c6d301e07000dd21560d",
+    "url": "/dgc/static/css/main.08966ec5.chunk.css"
+  },
+  {
+    "revision": "7bba80f7a21b750745e8",
+    "url": "/dgc/static/js/2.53adee8c.chunk.js"
+  },
+  {
+    "revision": "49423731f4b42bb1acb1fefa8741c67d",
+    "url": "/dgc/static/js/2.53adee8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d301e07000dd21560d",
+    "url": "/dgc/static/js/main.1e4ae4d8.chunk.js"
+  },
+  {
+    "revision": "65140b992c773d60f6da",
+    "url": "/dgc/static/js/runtime-main.ce4bd351.js"
+  }
+]);
